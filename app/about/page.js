130353c347c46db022d1bd9ef785e81e31b1f8cc
mdasifalijihat <mdasifalijihat@gmail.com>
2025-09-29 11:17:52 +0600

    added route

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <main className="mt-10">
+        This is the about page lorem ipsum dolor sit amet consectetur adipiscing
+        elit
+      </main>
+    </div>
+  );
+}
