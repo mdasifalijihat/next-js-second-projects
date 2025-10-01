26b4bafb5016574d8ce5affad8575b88baf915fe
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-01 21:19:31 +0600

    added navbar

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function page() {
+export default function Mission() {
   return (
     <div>
       <main className="mt-8">
-        lorem ipsum dolor sit amet consectetur adipiscing elit lorem20 
+        <h2 className="text-2xl font-bold mb-4">Our Mission</h2>
+        Our mission is to provide high-quality products and services that
+        exceed our customers' expectations. We strive to innovate and lead in
+        our industry, fostering a culture of excellence and integrity.
+        lorem ipsum dolor sit amet consectetur adipiscing elit lorem20
       </main>
     </div>
   );
